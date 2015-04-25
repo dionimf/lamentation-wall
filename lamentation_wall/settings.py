1640d580f8c443d44902b11e73ce645748eced73
Jesse Nazario <jessenzr@gmail.com>
2015-04-25 11:09:00 -0300

    use host db4free

--- lamentation_wall/settings.py
+++ lamentation_wall/settings.py
@@ -66,20 +66,26 @@ WSGI_APPLICATION = 'lamentation_wall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+if os.getenv('DB_ENGINE') == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
     }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'lamentation_wall',
-#         'USER': 'postgres',
-#     }
-# }
+else:
+    # 'django.db.backends.mysql'
+    # 'lamentation_wall'
+    # 'postgres'
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'HOST': os.getenv('DB_HOST'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
